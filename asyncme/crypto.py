fc46de4c9e9777f9a6276b875f79fd96f8d031f6
Matthew Ellison <matt@arroyonetworks.com>
2016-06-20 11:46:59 -0400

    Docstrings + Tests for `crypto` Module

--- asyncme/crypto.py
+++ asyncme/crypto.py
@@ -5,6 +5,7 @@ from math import ceil
 from enum import Enum
 
 from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat import backends
 
 from asyncme import utils
@@ -63,6 +64,12 @@ class AsymmetricKey:
         else:
             raise ValueError("Key does not appear to be valid PEM format.")
 
+    def __new__(cls, *args, **kwargs):
+        if cls is AsymmetricKey:
+            raise RuntimeError("You must not instantiate AsymmetricKey "
+                               "directly")
+        return super().__new__(cls)
+
     def __init__(self, key, *, key_alg, key_type):
         self._key = key
         self._alg = key_alg
@@ -82,28 +89,79 @@ class AsymmetricKey:
 
     @property
     def thumbprint(self):
-        raise NotImplementedError("Must be implemented by subclass")
+        """
+        Returns the SHA256 thumbprint of the key pair.
+
+        :return: ``bytes`` containing the key pair thumbprint digest.
+        """
+        raise NotImplementedError("Must be implemented by subclass")            # pragma: nocover # noqa
 
     @property
     def fingerprint(self):
+        """
+        Synonym for ``AsymmetricKey.thumbprint``.
+        """
         return self.thumbprint
 
     @property
     def jwk_thumbprint(self):
-        raise NotImplementedError("Must be implemented by subclass")
+        """
+        Returns the SHA256 thumbprint of the key pair JWK.
+
+        .. note::
+
+            This is different than the standard thumbprint.
+            See RFC7638.
+
+        :return: ``bytes`` containing the JWK thumbprint digest.
+        """
+        raise NotImplementedError("Must be implemented by subclass")            # pragma: nocover # noqa
 
     def __len__(self):
         return self.size
 
     def to_jwk(self):
+        """
+        Convert this key to its JSON Web Key (JWK) format.
+
+        :return: A ``dict`` representing this key in JWK format.
+        """
         return {
             "kty": KeyAlgorithm(self.algorithm).upper()
         }
 
 
 class PublicKey(AsymmetricKey):
+    """
+    Represents an RSA Public Key.
+    """
 
     def __init__(self, key):
+        """
+        Creates a new PublicKey object from a
+        ``cryptography.hazmat.primitives.asymmetric.rsa.RSAPublicKey``.
+
+        You should avoid instantiating this class directly unless you know
+        what you are doing. Use the loader methods on the ``AsymmetricKey``
+        parent class instead.
+
+        :param key: The underlying
+         ``cryptography.hazmat.primitives.asymmetric.rsa.RSAPublicKey`` this
+         public key represents.
+
+        :raises ValueError: If the given key is not an instance of the expected
+         key type.
+        """
+
+        # If the given key is private, get it's corresponding public key.
+        if isinstance(key, rsa.RSAPrivateKey):
+            key = key.public_key()
+
+        if not isinstance(key, rsa.RSAPublicKey):
+            raise ValueError("Key must be an instance of {}".format(
+                rsa.RSAPublicKey
+            ))
+
         super().__init__(
             key=key,
             key_alg=KeyAlgorithm.RSA,
@@ -136,6 +194,7 @@ class PublicKey(AsymmetricKey):
 
     def to_jwk(self):
         jwk = super().to_jwk()
+
         jwk['n'] = _prepare_key_num(self._key.public_numbers().n)
         jwk['e'] = _prepare_key_num(self._key.public_numbers().e)
 
@@ -143,8 +202,32 @@ class PublicKey(AsymmetricKey):
 
 
 class PrivateKey(AsymmetricKey):
+    """
+    Represents an RSA Private Key.
+    """
 
     def __init__(self, key):
+        """
+        Creates a new PrivateKey object from a
+        ``cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey``.
+
+        You should avoid instantiating this class directly unless you know
+        what you are doing. Use the loader methods on the ``AsymmetricKey``
+        parent class instead.
+
+        :param key: The underlying
+         ``cryptography.hazmat.primitives.asymmetric.rsa.RSAPrivateKey`` this
+         private key represents.
+
+        :raises ValueError: If the given key is not an instance of the expected
+         key type.
+        """
+
+        if not isinstance(key, rsa.RSAPrivateKey):
+            raise ValueError("Key must be an instance of {}".format(
+                rsa.RSAPrivateKey
+            ))
+
         super().__init__(
             key=key,
             key_alg=KeyAlgorithm.RSA,
@@ -163,9 +246,16 @@ class PrivateKey(AsymmetricKey):
 
     @property
     def public_key(self):
+        """
+        Return the corresponding public key for this private key.
+
+        :return: A ``PublicKey`` object corresponding to the public key that
+         is derived for this ``PrivateKey``.
+        """
         return self._public_key
 
     def to_jwk(self):
+
         jwk = self.public_key.to_jwk()
         jwk['d'] = _prepare_key_num(self._key.private_numbers().d)
         jwk['p'] = _prepare_key_num(self._key.private_numbers().p)
--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,6 +1,7 @@
 
 import os
 import tempfile
+import hashlib
 
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
@@ -15,6 +16,9 @@ from asyncme import crypto
 
 
 RSA_KEY_SIZE = 2048
+
+RSA_PUBLIC_JWK_KEYS = ['kty', 'e', 'n']
+RSA_PRIVATE_JWK_KEYS = RSA_PUBLIC_JWK_KEYS + ['p', 'qi', 'd', 'dp', 'q', 'dq']
 
 
 # --------------------------------------------------------------------------- #
@@ -87,7 +91,55 @@ def rsa_public_key_PEM_file(request, rsa_public_key):
     return tmp.name
 
 
+@pytest.fixture
+def random_data_file(request):
+
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+
+    with open("/dev/urandom", "rb") as in_file:
+        with tmp as out_file:
+            out_file.write(in_file.read(100))
+
+    def finalizer():
+        os.remove(tmp.name)
+
+    request.addfinalizer(finalizer)
+
+    return tmp.name
+
+
 # --------------------------------------------------------------------------- #
+
+# Generic Key Tests
+
+
+def test_cannot_instantiate_asymmetric_directly(rsa_public_key):
+
+    with pytest.raises(RuntimeError):
+        crypto.AsymmetricKey(
+            key=rsa_public_key,
+            key_alg=crypto.KeyAlgorithm.RSA,
+            key_type=crypto.KeyType.PUBLIC
+        )
+
+
+def test_invalid_pem_format(random_data_file):
+
+    with pytest.raises(ValueError):
+        crypto.AsymmetricKey.from_pem_file(
+            random_data_file
+        )
+
+
+def test_len_is_size(rsa_public_key):
+
+    key = crypto.PublicKey(rsa_public_key)
+    assert key.size == len(key)
+
+
+# --------------------------------------------------------------------------- #
+
+# RSA Key Tests
 
 
 def test_rsa_private_from_PEM_file(rsa_private_key_PEM_file):
@@ -116,3 +168,139 @@ def test_rsa_public_from_PEM_file(rsa_public_key_PEM_file):
 
     assert isinstance(key, crypto.PublicKey)
     assert key.type == crypto.KeyType.PUBLIC
+
+
+def test_rsa_private_from_obj(rsa_private_key):
+
+    key = crypto.PrivateKey(rsa_private_key)
+
+    assert isinstance(key, crypto.AsymmetricKey)
+    assert key.algorithm == crypto.KeyAlgorithm.RSA
+    assert key.size == RSA_KEY_SIZE
+
+    assert isinstance(key, crypto.PrivateKey)
+    assert key.type == crypto.KeyType.PRIVATE
+
+
+def test_rsa_private_from_public_obj(rsa_public_key):
+
+    with pytest.raises(ValueError):
+        crypto.PrivateKey(rsa_public_key)
+
+
+def test_rsa_private_from_bad_obj():
+
+    with pytest.raises(ValueError):
+        crypto.PrivateKey("NotValid")
+
+
+def test_rsa_public_from_obj(rsa_public_key):
+
+    key = crypto.PublicKey(rsa_public_key)
+
+    assert isinstance(key, crypto.AsymmetricKey)
+    assert key.algorithm == crypto.KeyAlgorithm.RSA
+    assert key.size == RSA_KEY_SIZE
+
+    assert isinstance(key, crypto.PublicKey)
+    assert key.type == crypto.KeyType.PUBLIC
+
+
+def test_rsa_public_from_private_obj(rsa_private_key):
+
+    key = crypto.PublicKey(rsa_private_key)
+
+    assert isinstance(key, crypto.AsymmetricKey)
+    assert key.algorithm == crypto.KeyAlgorithm.RSA
+    assert key.size == RSA_KEY_SIZE
+
+    assert isinstance(key, crypto.PublicKey)
+    assert key.type == crypto.KeyType.PUBLIC
+
+
+def test_rsa_public_from_bad_obj():
+
+    with pytest.raises(ValueError):
+        crypto.PublicKey("NotValid")
+
+
+def test_rsa_public_jwk(rsa_public_key):
+
+    key = crypto.PublicKey(rsa_public_key)
+    jwk = key.to_jwk()
+
+    assert isinstance(jwk, dict)
+    assert all(k in RSA_PUBLIC_JWK_KEYS for k in jwk.keys())
+
+    # JWK Thumbprint
+    assert isinstance(key.jwk_thumbprint, bytes)
+    assert len(key.jwk_thumbprint) * 8 == 256
+
+
+def test_rsa_private_jwk(rsa_private_key):
+
+    key = crypto.PrivateKey(rsa_private_key)
+    jwk = key.to_jwk()
+
+    assert isinstance(jwk, dict)
+    assert all(k in RSA_PRIVATE_JWK_KEYS for k in jwk.keys())
+
+    # Ensure that the Public Key's JWK is a Subset of the Private Key's JWK
+    pub_jwk = set(key.public_key.to_jwk().items())
+    assert pub_jwk.issubset(set(jwk.items()))
+
+    # JWK Thumbprint
+    assert isinstance(key.jwk_thumbprint, bytes)
+    assert len(key.jwk_thumbprint) * 8 == 256
+
+    # Public and Private Thumbprint should be the Same
+    assert key.jwk_thumbprint == key.public_key.jwk_thumbprint
+
+
+def test_rsa_private_thumbprint(rsa_private_key):
+
+    key = crypto.PrivateKey(rsa_private_key)
+
+    assert isinstance(key.thumbprint, bytes)
+    assert len(key.thumbprint) * 8 == 256
+
+    # `fingerprint` is a synonym to thumbprint
+    assert key.thumbprint == key.fingerprint
+
+    # Public and Private Thumbprint should be the Same
+    assert key.thumbprint == key.public_key.thumbprint
+
+    # Verify Hash Contents
+    pub_bytes = key.public_key._key.public_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PublicFormat.PKCS1
+    )
+
+    if isinstance(pub_bytes, str):
+        pub_bytes = pub_bytes.encode()
+
+    hash = hashlib.sha256(pub_bytes).digest()
+    assert key.thumbprint == hash
+
+
+def test_rsa_public_thumbprint(rsa_public_key):
+
+    key = crypto.PublicKey(rsa_public_key)
+
+    assert isinstance(key.thumbprint, bytes)
+    assert len(key.thumbprint) * 8 == 256
+
+    # `fingerprint` is a synonym to thumbprint
+    assert key.thumbprint == key.fingerprint
+
+    # Verify Hash Contents
+    pub_bytes = rsa_public_key.public_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PublicFormat.PKCS1
+    )
+
+    if isinstance(pub_bytes, str):
+        pub_bytes = pub_bytes.encode()
+
+    hash = hashlib.sha256(pub_bytes).digest()
+    assert key.thumbprint == hash
